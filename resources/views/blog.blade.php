9bfaa624b71a6ef1b1b76019573c8abd5c7b9dcc
Fitria Nurhaliza <fitrianrhlz0@gmail.com>
2024-12-11 11:49:06 +0700

    Laravel latihan

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blog</title>
+</head>
+<body>
+    <h1>Blog Articles</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,42 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Home Page';
-});
-
 Route::get('/signin', function () {
-    return 'Signin Form';
-});
-
-Route::post('/signin', function () {
-    return 'Processing Signin Data';
+    return view('signin');
 });
 
 Route::get('/signup', function () {
-    return 'Signup Form';
-});
-
-Route::post('/signup', function () {
-    return 'Processing Signup Data';
+    return view('signup');
 });
 
 Route::get('/blog', function () {
-    return 'Blog Articles List';
+    return view('blog');
 });
 
-Route::get('/blog/{slug}', function ($slug) {
-    return 'Detail Article with slug: ' . $slug;
-});
+Route::get('/profile', function () {
+    return view('profile');
 
-Route::get('/category/{slug}', function ($slug) {
-    return 'Articles in category with slug: ' . $slug;
-});
-
-Route::get('/author/{username}', function ($username) {
-    return 'Articles by author: ' . $username;
-});
-
-Route::get('/privacy-policy', function () {
-    return 'Privacy Policy Page';
+    
 });
